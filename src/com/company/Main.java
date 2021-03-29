d4f8389227749788cee7108b3ea34fa1573c5457
xach1234567890 <xach.hakobyan.11@mail.ru>
2021-03-29 21:16:05 +0400

    commit v1

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte b = 1;
+        short s = 10;
+        int i = 1000;
+        long l = 102;
+        float f = 12.412f;
+        double d = 2.4;
+        boolean bool = false;
+        char c = 'H';
+
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(bool);
+        System.out.println(c);
+    }
+}
