e5e9b2c8e0c60adb6e4090876bd0751850188349
xach1234567890 <xach.hakobyan.11@mail.ru>
2021-04-02 16:21:40 +0400

    Homework N2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,22 +3,48 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        byte b = 1;
-        short s = 10;
-        int i = 1000;
-        long l = 102;
-        float f = 12.412f;
-        double d = 2.4;
-        boolean bool = false;
-        char c = 'H';
+        // n1
+        int[] a1 = new int[]{1, 2};
 
-        System.out.println(b);
-        System.out.println(s);
-        System.out.println(i);
-        System.out.println(l);
-        System.out.println(f);
-        System.out.println(d);
-        System.out.println(bool);
-        System.out.println(c);
+        // n2
+        int[] a2 = new int[1000];
+        for (int i = 1; i < 1001; i++)
+            a2[i - 1] = i;
+
+        // n3
+        int[] a3 = new int[10];
+        for (int i = 1, j = 0; i < 20; i += 2)
+            a3[j++] = i;
+
+        // n4
+        int[] a4 = new int[20];
+        for (int i = 0; i < 20; i++)
+            a4[i] = i;
+        for (int i = 0; i < a4.length; i++)
+            if (a4[i] % 5 == 0)
+                System.out.println(a4[i]);
+
+        // n5
+        int[] a5 = new int[1000];
+        for (int i = 0; i < 1000; i++)
+            a5[i] = i;
+        for (int i = 0; i < a5.length; i++)
+            if (a5[i] > 24 && a5[i] < 468)
+                System.out.println(a5[i]);
+
+        // n6
+        int[] a6 = new int[20];
+        int n1 = 0;
+        for (int i = 1; i < 21; i++)
+            a6[i-1] = i;
+        for (int i = 0; i < a6.length; i++)
+            if (a6[i] % 2 == 0)
+                n1++;
+        System.out.println(n1);
+
+        // n7
+        int n2 = 2;
+        for (int i = 1; i < 11; i++)
+            System.out.println(n2 + "x" + i + " = " + n2 * i);
     }
 }
