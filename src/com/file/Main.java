e402be372f9d38243c4bba65bb43540f66b7ac26
xach1234567890 <xach.hakobyan.11@mail.ru>
2021-04-14 01:10:12 +0400

    filenames

---
+++ src/com/file/Main.java
@@ -0,0 +1,37 @@
+package com.file;
+
+import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String path = "C:/Users/xachh/OneDrive/Рабочий стол/1";
+        File file = new File(path);
+
+        showFileName(file);
+    }
+
+    public static void showFileName(File file){
+        List<File> list = new LinkedList<>();
+        list.add(file);
+
+        while (!list.isEmpty()){
+            File last = list.get(list.size()-1);
+
+            if (last.isDirectory()){
+                for (File f : Objects.requireNonNull(last.listFiles())) {
+                    if (f.isDirectory())
+                        list.add(f);
+                    else
+                        System.out.println(f.getName());
+                }
+            }
+
+            System.out.println(last.getName());
+            list.remove(last);
+        }
+    }
+}
